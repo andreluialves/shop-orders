c253c1314595faf15f70bea6e4d115f56408f407
André Alves <andreluialves@hotmail.com>
2026-07-09 15:06:22 -0300

    feat: add product entity

--- internal/domain/product.go
+++ internal/domain/product.go
@@ -1,1 +1,24 @@
 package domain
+
+type Product struct {
+	ID       string
+	Name     string
+	Price    float64
+	Quantity int
+}
+
+func (p *Product) UpdateName(newName string) {
+	p.Name = newName
+}
+
+func (p *Product) UpdatePrice(newPrice float64) {
+	p.Price = newPrice
+}
+
+func (p *Product) RestoreQuantity(quantity int) {
+	p.Quantity += quantity
+}
+
+func (p *Product) ReduceQuantity(quantity int) {
+	p.Quantity -= quantity
+}
