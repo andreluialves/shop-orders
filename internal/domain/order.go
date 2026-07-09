8ce1146b006fbf300166d303d3e4a065797b709b
André Alves <andreluialves@hotmail.com>
2026-07-09 16:47:09 -0300

    feat: add order entity

--- internal/domain/order.go
+++ internal/domain/order.go
@@ -1,1 +1,57 @@
 package domain
+
+type OrderStatus string
+
+const (
+	OrderStatusPending  OrderStatus = "PENDING"
+	OrderStatusPaid     OrderStatus = "PAID"
+	OrderStatusCanceled OrderStatus = "CANCELED"
+)
+
+type OrderItem struct {
+	Product  *Product
+	Quantity int
+	Price    float64
+}
+
+func NewOrderItem(product *Product, quantity int, price float64) *OrderItem {
+	return &OrderItem{
+		Product:  product,
+		Quantity: quantity,
+		Price:    price,
+	}
+}
+
+type Order struct {
+	ID      string
+	Cliente string
+	Items   []*OrderItem
+	Status  OrderStatus
+}
+
+func NewOrder(id string, cliente string) *Order {
+	return &Order{
+		ID:      id,
+		Cliente: cliente,
+		Items:   []*OrderItem{},
+		Status:  OrderStatusPending,
+	}
+}
+
+func TotalSum(orders []*Order) float64 {
+	var total float64
+	for _, order := range orders {
+		for _, item := range order.Items {
+			total += item.Price * float64(item.Quantity)
+		}
+	}
+	return total
+}
+
+func PayOrder(order *Order) {
+	order.Status = OrderStatusPaid
+}
+
+func CancelOrder(order *Order) {
+	order.Status = OrderStatusCanceled
+}
