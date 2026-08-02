0eb4ee1c93bd49f8942f9546d44983d375248475
André Alves <andreluialves@hotmail.com>
2026-08-02 20:07:55 -0300

    test: adds test suite for domain, service, and controllers

---
+++ internal/controllers/mocks_test.go
@@ -0,0 +1,44 @@
+package controllers_test
+
+import "github.com/andreluialves/shop-orders/internal/domain"
+
+type mockProductRepository struct {
+	FindByIDFunc func(id string) (*domain.Product, error)
+	SaveFunc     func(product *domain.Product) error
+	ListFunc     func() ([]*domain.Product, error)
+}
+
+func (m *mockProductRepository) FindByID(id string) (*domain.Product, error) {
+	return m.FindByIDFunc(id)
+}
+
+func (m *mockProductRepository) Save(product *domain.Product) error {
+	return m.SaveFunc(product)
+}
+
+func (m *mockProductRepository) List() ([]*domain.Product, error) {
+	return m.ListFunc()
+}
+
+type mockOrderRepository struct {
+	FindByIDFunc func(id string) (*domain.Order, error)
+	SaveFunc     func(order *domain.Order) error
+	UpdateFunc   func(order *domain.Order) error
+	ListFunc     func(limit, offset int) ([]*domain.Order, int, error)
+}
+
+func (m *mockOrderRepository) FindByID(id string) (*domain.Order, error) {
+	return m.FindByIDFunc(id)
+}
+
+func (m *mockOrderRepository) Save(order *domain.Order) error {
+	return m.SaveFunc(order)
+}
+
+func (m *mockOrderRepository) Update(order *domain.Order) error {
+	return m.UpdateFunc(order)
+}
+
+func (m *mockOrderRepository) List(limit, offset int) ([]*domain.Order, int, error) {
+	return m.ListFunc(limit, offset)
+}
